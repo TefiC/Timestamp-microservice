75f68f2978284ce7ca9463126dd5fee6fec7a6d2
Estefania Cass <TefiC@users.noreply.github.com>
2017-05-04 19:13:56 +0000

    Added conditions to check the string formatting and set  unix time to be in seconds. Update console.log statement on app.listen

--- server.js
+++ server.js
@@ -1,37 +1,64 @@
+/*
+ * Required modules
+ */
 var express = require("express");
-var app = express();
-
+var url = require("url");
 var moment = require("moment");
 
-var date = process.argv[2]; //'December%20,%202015'; //process.argv[2];
+/*
+ * A
+ */
+var app = express();
 
-app.get('/', function (req, res) {
-    
-    var isValid = moment(date, 'MMMM%20DD,%20YYYY', true).isValid(); 
-    
-    if (isValid) {
-        
-        var naturalTime = date.split("%20").join(' ');
-        var unixTime = new Date(naturalTime).getTime();
-        
-    } else {
-        
-        naturalTime = null;
-        unixTime = null;
-        
-    }
-    
-    var JSONres = {
-        "unix" : unixTime,
-        "natural" : naturalTime
-    }
-    
-    res.send(JSON.stringify(JSONres));
+app.get('/*', function (req, res) {
+	
+	//Extract parameters from URL
+	var date = req.params[0];
+	var month = date.split(' ')[0];
+	
+	//Check if the date format is valid 
+	var isValid = moment(date, 'MMM DD, YYYY').isValid(); 
+	
+	// If the format is valid and the month is a string
+	if (isValid == true && isNaN(month)) {
+		
+		// If the date is a string, split by the space character
+		// and join them with a space, then calculate the date in UNIX
+		if (isNaN(date) == true) {
+			
+			var naturalTime = date;
+			
+			//Get the unix time and convert it to seconds
+			var unixTime = (new Date(naturalTime).getTime())/1000;
+			
+		// Else, if its a number, a unix format,
+		// convert the unix time to the right format
+		} else {
+			
+			var unixTime = date;
+			var naturalTime = moment.unix(unixTime).format("MMMM DD, YYYY");
+		}
+	  
+	//Else, if the format is not valid 
+	} else {
+		
+		var naturalTime = null;
+		var unixTime = null;
+		
+	}
+	
+	//Create the JSON object, and then send it back to the client
+	var JSONres = {
+		"unix" : unixTime,
+		"natural" : naturalTime
+	}
+	
+	res.send(JSON.stringify(JSONres));
    
 });
 
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening!');
+	console.log('Example app listening to current port!');
 });
 
 
