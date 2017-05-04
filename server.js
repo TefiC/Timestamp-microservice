a4c3811a2fe9310de5f85cf40c57fcd4a896b50a
Estefania Cass <TefiC@users.noreply.github.com>
2017-05-04 15:46:33 +0000

    Added the moment.js module and a handler for null requests

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 var express = require("express");
-var app = express()
+var app = express();
+
+var moment = require("moment");
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-})
+    
+    var isValid = moment('December%2015,%202015', 'MMMM%20DD,%20YYYY', true).isValid(); 
+    
+    if (isValid) {
+        res.send('Hello World!' + isValid.toString());
+    } else {
+        var JSONres = {
+            "unix" : null,
+            "natural" : null 
+        }
+        res.send(JSON.stringify(JSONres));
+    }
+   
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
-})
+});
+
+
+
+
